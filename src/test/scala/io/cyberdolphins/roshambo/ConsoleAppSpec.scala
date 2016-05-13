97368215612987d901856c4f142cf0a1a0772534
mwielocha <mwielocha@icloud.com>
2016-05-13 10:49:13 +0200

    more ui testing

--- src/test/scala/io/cyberdolphins/roshambo/ConsoleAppSpec.scala
+++ src/test/scala/io/cyberdolphins/roshambo/ConsoleAppSpec.scala
@@ -5,6 +5,9 @@ import org.scalatest.{FlatSpec, MustMatchers}
 import io.cyberdolphins.roshambo.views._
 import scala.collection.mutable
 
+import scala.util.{Try, Success, Failure}
+
+
 /**
  * Created by Mikolaj Wielocha on 12/05/16
  */
@@ -39,7 +42,10 @@ class ConsoleAppSpec extends FlatSpec with MustMatchers {
 
     val consoleApp: View = ConsoleApp()
 
-    consoleApp()
+    Try(consoleApp()) match {
+      case Failure(e) => e.printStackTrace()
+      case Success(_) =>
+    }
 
     val visitedScreens = out.screenBuffer.toList
   }
@@ -69,7 +75,7 @@ class ConsoleAppSpec extends FlatSpec with MustMatchers {
     """).map(_.stripMargin.trim)
   }
 
-  it should "go from the welcome menu to game menu and back" in new Interaction("1", "0") {
+  it should "go from the welcome menu to games menu and back" in new Interaction("1", "0") {
 
     visitedScreens mustBe List(
     """
@@ -87,6 +93,43 @@ class ConsoleAppSpec extends FlatSpec with MustMatchers {
       | 1. Rock Paper Scissors
       | 2. Rock Paper Scissors Lizard Spock
       | 0. Exit
+    """).map(_.stripMargin.trim)
+  }
+
+   it should "go from the welcome menu to game menu and play one round" in new Interaction("1", "1", "1", "2", "2") {
+
+    visitedScreens mustBe List(
+    """
+      | 1. Rock Paper Scissors
+      | 2. Rock Paper Scissors Lizard Spock
+      | 0. Exit
+    """,
+    """
+      | 1. Player vs Player
+      | 2. Player vs Computer
+      | 3. Computer vs Computer
+      | 0. Back
+    """,
+    """
+      | 1. Paper
+      | 2. Rock
+      | 3. Scissors
+    """,
+    """
+      | Rock vs Scissors
+      | Player1 wins!
+    """,
+    """
+      | Play again?
+      |
+      | 1. Yes
+      | 2. No
+    """,
+    """
+      | 1. Player vs Player
+      | 2. Player vs Computer
+      | 3. Computer vs Computer
+      | 0. Back
     """).map(_.stripMargin.trim)
   }
 }
